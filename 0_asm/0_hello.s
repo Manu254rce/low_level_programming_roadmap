f2f91a99b0f0c5dca566cb4aeae6d8121f1323c5
Manu254rce <straussmoabit@gmail.com>
2024-03-30 19:51:03 +0300

    Hello world program in assembly

--- 0_asm/0_hello.s
+++ 0_asm/0_hello.s
@@ -1,13 +1,17 @@
+// function that prints "Hello World to STDOUT and then calls sys_exit"
+
 .global _start
 .intel_syntax noprefix
 
 _start:
+	// prints the "Hello World to STDOUT"
 	mov rax, 1
 	mov rdi, 1
 	lea rsi, [hello_world]
 	mov rdx, 13
 	syscall
-
+	
+	// perform a system exit instruction
 	mov rax, 60
 	mov rdi, 69
 	syscall
