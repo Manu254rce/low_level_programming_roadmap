c380b16bd43f7da3d0a3d70d3a3ab6443d34b4e2
Z'PHYR <emmanuellamira20@outlook.com>
2025-05-02 15:46:41 +0300

    chore: moved the 32-bit ASM code into the 32-bit dir

--- 0_asm/32-bit/src/0-exit_with_arg.asm
+++ 0_asm/32-bit/src/0-exit_with_arg.asm
@@ -1,4 +1,4 @@
-; Assembly code that exits a program with status 1
+; Assembly code that exits a program with status 1 in a 32-bit system
  
 ; MOV eax, 1 - Run the EXIT syscall
 ; MOV ebx, 1 - Select the type of exit code (exit code 1)
@@ -8,10 +8,10 @@
 ; here, we define a global label called _start, indicating that it can
 ; be accessed from outside the program
 section .text
-	global _exit
+	global _start
 
 ; and this _start label defines the entry point of the program (similar to main in C)
-_exit:
+_start:
 	MOV eax,1
 	MOV ebx,1
 	INT 80h
